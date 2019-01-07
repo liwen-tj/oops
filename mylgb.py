4f6828e0ff6ababcbb2ca21d30f9a9939eef0ef5
liwen-tj <3407617286@qq.com>
2019-01-07 20:34:54 +0800

    first commit

---
+++ mylgb.py
@@ -0,0 +1,49 @@
+import lightgbm as lgb
+import pandas as pd
+from sklearn import preprocessing
+
+
+def read_data(filename):
+    data = pd.read_csv(filename).sample(frac=1)
+    # 打乱数据
+    Xs = data[["F1","F2","F3","F4","F5","F6"]].values.astype(float)
+    X_scaled = preprocessing.MinMaxScaler().fit_transform(Xs)
+    Ys = data.RMSD.values
+    # X_scaled（二维数组）, Ys（一维数组）都是ndarray
+    
+    B = 40000 # Train Num
+    T = 45730
+    (trainX, testX) = (X_scaled[:B], X_scaled[B:T])
+    (trainY, testY) = (Ys[:B], Ys[B:T])
+    return (trainX, testX, trainY, testY)
+
+
+def train(trainX, trainY):
+	# 模型
+    params = {
+        'learning_rate': 0.1,
+        'objective': 'regression_l1',
+        'max_depth': 3,
+        'bagging_fraction': 0.5,
+        'min_data_in_leaf': 50,
+        'metric': 'mse',
+        'seed': 42
+    }
+    train_data = lgb.Dataset(trainX, label=trainY)
+    bst = lgb.train(params, train_data, num_boost_round=500)
+    return bst
+
+
+if __name__ == '__main__':
+    # instances = 45730
+    (trainX, testX, trainY, testY) = read_data("CASP.csv")
+    bst = train(trainX, trainY)
+    ypred = bst.predict(testX)
+    for i in range(100):
+        print(ypred[i], testY[i])
+    from sklearn.metrics import mean_squared_error, mean_absolute_error
+    mse = mean_squared_error(testY, ypred)
+    mae = mean_absolute_error(testY, ypred)
+    print('----------------------')
+    print(mse, mae)
+    
---
+++ regBoost.py
@@ -0,0 +1,142 @@
+from random import sample
+from sklearn import linear_model
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+
+class RegBoost:
+    feature_num = 2 # 每个线性回归的特征数目
+    learner_num = 10 # 有多少个学习器可供选择
+    learning_rate = 0.5 # 学习速率
+    max_layer = 4 # 最大递归次数（最多经过的学习器个数）
+    min_sample = 200 # 数目少于min_sample个时则不再进行线性回归
+    Knn = 11
+    times_temp = 0
+    def __init__(self, train_data, labels):
+        self.train_data = train_data # shape = (instances, attributes)
+        self.labels = labels
+        self.attribute_num = len(train_data[0])
+
+
+    def get_feature(self):
+        '''选择特征组，shape=(learner_num, feature_num)'''
+        features = set() # 集合类型数据
+        while features.__len__() != self.learner_num:
+            c = tuple(sorted(sample(range(self.attribute_num), self.feature_num)))
+            features.add(c) # 集合元素不可以add列表类型的数据，所以上一步转为tuple
+        return list(features) # 集合类型不可以索引，还要再转成列表类型
+
+
+    def get_learner(self, fea, data, Y):
+        '''根据选定的特征，还有数据，得到基学习器'''
+        """!!! 注意修改Y值 !!! fea, data, Y都是list数据"""
+        #1 取出数据
+        dataX = self.train_data[:,fea][data,:]
+        #2 基学习器
+        regr = linear_model.LinearRegression()
+        regr.fit(dataX, Y)
+        pred = regr.predict(dataX)
+        #3 分出正负组，并修改Y值
+        n = data.__len__()
+        data_p, data_n, Yp, Yn = [], [], [], []
+        for i in range(n):
+            if(pred[i] > Y[i]):
+                data_p.append(data[i])
+                Yp.append(Y[i] - self.learning_rate*pred[i])
+            else:
+                data_n.append(data[i])
+                Yn.append(Y[i] - self.learning_rate*pred[i])
+        return (regr, data_p, data_n, Yp, Yn)
+
+
+    def get_model(self, data, Y, layer):
+        print('get_model layer =', layer, 'times =', self.times_temp)
+        self.times_temp += 1
+        # 选择特征
+        features = self.get_feature()
+        model = []
+        for i in range(self.learner_num):
+            fea = features[i]
+            (learner, data_p, data_n, Yp, Yn) = self.get_learner(fea, data, Y)
+            if(layer-1 == 0 or data_p.__len__() < self.min_sample): # 已经达到最大层数
+                model_p = None
+            else:
+                model_p = self.get_model(data_p, Yp, layer-1)
+
+            if(layer-1 == 0 or data_n.__len__() < self.min_sample): # 已经达到最大层数
+                model_n = None
+            else:
+                model_n = self.get_model(data_n, Yn, layer-1)
+            
+            # data_p, data_n, Yp, Yn都是list数据类型
+            model.append((learner, fea, data_p, data_n, model_p, model_n))
+        return model
+
+
+    def fit(self):
+        data_id = range(len(self.train_data))
+        # 包含了所有的基学习器，还有每个基学习器分出的数据索引
+        self.model = self.get_model(data_id, self.labels, self.max_layer)
+        print('fit done...')
+    
+
+    def pn_samples_dis(self, td, fea, datap, datan):
+        '''计算datap和datan所有数据，最近的K个在正负数据类中的分布'''
+        (kp, kn) = (0, 0)
+        #1 联合所有数据，计算最近的K个
+        data = datap + datan
+        samples = self.train_data[:,fea][data,:]
+        neigh = NearestNeighbors(n_neighbors=self.Knn)
+        neigh.fit(samples)
+        (dist, ind) = neigh.kneighbors([[td[f] for f in fea]])
+        (dist, ind) = (dist[0], ind[0])
+        #2 确定这K个最近点的分布如何
+        for i in ind:
+            if i < datap.__len__():
+                kp += 1
+            else:
+                kn += 1
+        #3 Done
+        #print("正负值：", kp, kn)
+        return kp - kn
+
+
+    def select_best_learner(self, model, td):
+        '''计算该层模型中最合适的基学习器'''
+        (idx, pos, maxd) = (0, 0, 0)
+        for i in range(self.learner_num):
+            # 计算正负差
+            d = self.pn_samples_dis(td, model[i][1], model[i][2], model[i][3])
+            if abs(d) > maxd:
+                (idx, pos, maxd) = (i, 0 if d > 0 else 1, abs(d))
+        #print('max distance =', maxd)
+        return (idx, pos)
+
+    
+    def predict(self, test_data, testY):
+        '''test_data(shape=(instances, attributes))是ndarray，返回值也应该是ndarray'''
+        results = []
+        y = 0
+        for td in test_data:
+            pred = []
+            model_curre = self.model
+            while True:
+                #1 选定基学习器 返回的是学习器的idx(list元素的index)和下一个学习器的正(0)负(1)
+                (idx, pos) = self.select_best_learner(model_curre, td)
+                #2 计算当前基学习器的预测值
+                regr = model_curre[idx][0]
+                feas = model_curre[idx][1]
+                y_t = regr.predict(np.array([[td[i] for i in feas]]))[0]
+                #3 判断下一层是否为空，空则结束
+                model_curre = model_curre[idx][4+pos] # pos正(0)负(1)
+                if model_curre is not None:
+                    pred.append(y_t * self.learning_rate)
+                else:
+                    pred.append(y_t)
+                    break
+            print(sum(pred), '\t', testY[y])
+            results.append(sum(pred))
+            y += 1
+    
+        return results
+    
